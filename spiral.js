343fc3fc6d0516c4fb1c2c01fd099358b033e975
Karkhi Mirza <123919709+Meyrza@users.noreply.github.com>
2025-07-24 18:31:59 +0700

    Add files via upload

--- spiral.js
+++ spiral.js
@@ -1,30 +1,28 @@
 function spiral(n) {
-  let num = 0
+  let num = 0;
   const matrix = Array.from({ length: n }, () =>
     Array.from({ length: n }, () => num++)
   )
 
-  const result = []
+  const result = [];
   let atas = 0, bawah = n - 1
   let kiri = 0, kanan = n - 1
 
-  while (atas <= bawah && kiri <= kanan) {
-    for (let i = kiri; i <= kanan; i++) result.push(matrix[atas][i])
-    atas++
+  while (result.length < n * n) {
+    for (let i = kiri; i <= kanan; i++) result.push(matrix[atas][i]);
+    atas++;
 
-    for (let i = atas; i <= bawah; i++) result.push(matrix[i][kanan])
-    kanan--
+    for (let i = atas; i <= bawah; i++) result.push(matrix[i][kanan]);
+    kanan--;
 
-    if (atas <= bawah)
-      for (let i = kanan; i >= kiri; i--) result.push(matrix[bawah][i])
-    bawah--
+    for (let i = kanan; i >= kiri; i--) result.push(matrix[bawah][i]);
+    bawah--;
 
-    if (kiri <= kanan)
-      for (let i = bawah; i >= atas; i--) result.push(matrix[i][kiri])
-    kiri++
+    for (let i = bawah; i >= atas; i--) result.push(matrix[i][kiri]);
+    kiri++;
   }
 
-  console.log(result)
+  console.log(result);
 }
 
 spiral(5)
